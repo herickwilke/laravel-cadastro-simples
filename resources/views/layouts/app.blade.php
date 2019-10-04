1e25fa27c7b4dc7f35add97bb2579dba84b77984
Herick <herick@weecode.com.br>
2019-10-04 16:14:51 -0300

    Navbar atualizado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div class="container">
-            @component('navbar')
+            @component('navbar', ["current"=>$current])
             @endcomponent
             <main role="main">
                 @hasSection('body')
